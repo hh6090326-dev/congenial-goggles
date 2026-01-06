3fbc4e3e48ad8409f6e8e08116414c6c39740865
hh6090326-dev <hh6090326@gmail.com>
2026-01-06 17:46:13 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,13 +1,9 @@
-import { TelegramClient } from "telegram";
-import { StringSession } from "telegram/sessions";
+import fetch from 'node-fetch';
 import 'dotenv/config';
 
-const miniBotToken = process.env.MINI_BOT_TOKEN;
-
 export async function checkJoin(userId) {
-  // Call Telegram API via mini-bot
   try {
-    const res = await fetch(`https://api.telegram.org/bot${miniBotToken}/getChatMember?chat_id=${process.env.UPDATE_CHANNEL}&user_id=${userId}`);
+    const res = await fetch(`https://api.telegram.org/bot${process.env.MINI_BOT_TOKEN}/getChatMember?chat_id=${process.env.UPDATE_CHANNEL}&user_id=${userId}`);
     const data = await res.json();
     if (data.ok && ['member','administrator','creator'].includes(data.result.status)) return true;
     return false;
